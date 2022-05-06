fbaf7e2f460c388d71b2d1cbb13a38d4b32e6950
ThomasBench <57524567+ThomasBench@users.noreply.github.com>
2022-05-06 17:34:12 +0200

    Add for several files

--- helpers.py
+++ helpers.py
@@ -1,28 +1,38 @@
 import pandas as pd 
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+from driver import DRIVER
+
+def songs_to_df(songArray): 
+    return pd.DataFrame([s.__dict__ for s in songArray])
+def get_source(link): 
+    DRIVER.get(link)
+    return DRIVER.page_source
+def genre_extractor(filepath):
+    dico = {}
+    with open(filepath, "r", encoding = "utf-8") as f:
+        data = BeautifulSoup(f, 'lxml').findAll('a')
+    for elem in data:
+        value = int(elem["value"])
+        genre = elem.findAll("span")[0].text
+        nb_songs = int(elem.findAll("span")[1].text.replace(',', ''))
+        dico[genre] = { "value":value, "nb_songs" : nb_songs}
+    return dico
 
 
-class Song:
-    "Small Song class to store all the infos of a specific song, might evolve later"
-    def __init__(self, songName, author, link, lyrics, chords):
-        self.name = songName
-        self.author = author
-        self.link = link
-        self.lyrics = lyrics
-        self.chords = chords
-    
+GENRE_DICT = genre_extractor("./data/genre_selector.html")
+
+def create_link(page,genre, date):
+    return f"https://www.ultimate-guitar.com/explore?decade[]={date}&genres[]={GENRE_DICT[genre]['value']}&page={page}&type[]=Chords"
 
 
-def songs_to_df(songArray): pd.DataFrame([s.__dict__ for s in songArray])
-def get_source(driver,link): driver.get(link).page_source
+def pageExist(link):
+    return len(BeautifulSoup(get_source(link), "lxml").findAll("div", class_ = "b-error-page wallpaper")) == 0
 
-
-
-def get_songs_from_link(driver, link):
+def get_songs_from_link( link):
 
     # 1. Retrieve the content of the landing page 
-    contentTable = BeautifulSoup(get_source(driver,link), 'lxml').findAll("div", class_ = "_3uKbA")
+    contentTable = BeautifulSoup(get_source(link), 'lxml').findAll("div", class_ = "_3uKbA")
 
     # 2. Initialise the song array 
     songs = []
@@ -32,16 +42,16 @@ def get_songs_from_link(driver, link):
         songName = content.find("a", class_ = "_3DU-x JoRLr _3dYeW").text
         artist = content.find('a',"_3DU-x hn34w _3dYeW").text
         link = content.find('a',"_3DU-x JoRLr _3dYeW")['href']
-        lyrics, chords = get_lyrics_chords_from_link(driver, link)
+        lyrics, chords = get_lyrics_chords_from_link( link)
         songs.append(Song(songName, artist, link, lyrics, chords))
     
     return songs
 
 
-def get_lyrics_chords_from_link(driver, link):
+def get_lyrics_chords_from_link( link):
 
     # 1. Retrieve the content of the song page
-    songData  = BeautifulSoup(get_source(driver,link)).find("pre", class_ = "_3F2CP _3hukP")        # The _3F2CP _3hukP class seems to refer to the song content of the page
+    songData  = BeautifulSoup(get_source(link), "lxml").find("pre", class_ = "_3F2CP _3hukP")        # The _3F2CP _3hukP class seems to refer to the song content of the page
 
     # 2. Initialise the lyrics and chords arrays
     lyrics = ""
@@ -55,3 +65,17 @@ def get_lyrics_chords_from_link(driver, link):
             lyrics += lyricsChords[1].text 
     
     return lyrics,chords
+
+
+
+
+
+class Song:
+    "Small Song class to store all the infos of a specific song, might evolve later"
+    def __init__(self, songName, author, link, lyrics, chords):
+        self.name = songName
+        self.author = author
+        self.link = link
+        self.lyrics = lyrics
+        self.chords = chords
+    
